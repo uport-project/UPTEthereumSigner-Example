1a712dbf8bba541667f22f8b798cf9c298a4fd57
Joshua Bell <belljh@gmail.com>
2018-07-12 19:26:00 -0400

    example app functions

---
+++ UPTEthereumSigner-Example/UPTEthereumSigner-Example/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UPTEthereumSigner-Example
+//
+//  Created by mac on 6/20/18.
+//  Copyright © 2018 Concensys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "UPTAppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([UPTAppDelegate class]));
+    }
+}
